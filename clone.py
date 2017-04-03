0f685db4961703ee540107a8683de55434c46277
Dean Liu <windsurf_dean@yahoo.com>
2017-04-03 20:28:28 +0000

    increased model conv layers, filter depth, stride size

--- clone.py
+++ clone.py
@@ -15,16 +15,15 @@ with open(csv_file) as f:
   for line in reader:
     lines.append(line)
 
-images = []
+crop_hgt = 40
+images=[]
 measurements = []
-# print('lines[0:5] = ', lines[0:5])
 for line in lines:
-  # print('line = ',line)
+ 
   source_path = line[0] #center channel video
-  # print('source_path = ', source_path)
   filename = source_path.split('/')[-1] #remove intermediate path
   image_file = data_path + 'IMG/' + filename
-  image = cv2.imread(image_file)
+  image = cv2.imread(image_file)[crop_hgt:]
   images.append(image)
 
   measurement = float(line[3]) #steering angle
@@ -33,29 +32,54 @@ for line in lines:
 X_train = np.array(images)
 y_train = np.array(measurements)
 
+# generate flipped image data
+X_train_flip = np.array(np.fliplr(images))
+y_train_flip = -np.array(measurements)
+
+X_train = np.concatenate((X_train, X_train_flip), axis=0)
+y_train = np.concatenate((y_train, y_train_flip), axis=0)
+
 # train model  
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, MaxPooling2D
 from keras.layers.convolutional import Conv2D
 
 model = Sequential()
-
-model.add(Lambda(lambda x: (x/255.0)-0.5, input_shape=(160,320,3)))
+imshape = images[0].shape
+model.add(Lambda(lambda x: (x/255.0)-0.5, input_shape=imshape))
 model.add(Conv2D(input_shape=(160,320,3),
-                 nb_filter=6,
+                 nb_filter=24,
+                 nb_row=8,
+                 nb_col=8,
+                 subsample=(2,2),
+                 border_mode='valid',
+                 activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(nb_filter=36,
+                 nb_row=5,
+                 nb_col=5,
+                 subsample=(2,2),
+                 border_mode='valid',
+                 activation='relu'))
+model.add(Conv2D(nb_filter=48,
                  nb_row=5,
                  nb_col=5,
                  subsample=(1,1),
                  border_mode='valid',
                  activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Conv2D(nb_filter=6,
+model.add(Conv2D(nb_filter=64,
                  nb_row=5,
                  nb_col=5,
                  subsample=(1,1),
                  border_mode='valid',
                  activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(nb_filter=64,
+                 nb_row=5,
+                 nb_col=5,
+                 subsample=(1,1),
+                 border_mode='valid',
+                 activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
 model.add(Dense(120, activation='relu'))
 model.add(Dense(84, activation='relu'))
@@ -66,7 +90,8 @@ model.compile(loss='mse',
 model.fit(X_train, y_train,
           validation_split=0.2,
           shuffle=True,
-          nb_epoch=8)
+          nb_epoch=8,
+          batch_size=64)
 
 model.save('model.h5')
 
