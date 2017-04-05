bfc7be1e3fd9e268c9b9b212f859613d00c9d94a
Dean Liu <windsurf_dean@yahoo.com>
2017-04-05 08:08:30 +0000

    updated clone.py

--- clone.py
+++ clone.py
@@ -8,8 +8,7 @@ import numpy as np
 data_path = './training_data/' # AWS sim data
 csv_file = data_path + 'driving_log.csv'
 
-crop_hgt = 40
-steer_corr = 0.02 # [normalized +/-1.0] (+) right
+steer_corr = 0.05 # [normalized +/-1.0] (+) right
 
 images=[]
 measurements = []
@@ -18,25 +17,26 @@ with open(csv_file) as f:
   reader = csv.reader(f)
   next(reader) # skip 1st line
   for line in reader:
+    steering_angle = float(line[3])
+
     # center channel
-    steering_angle = float(line[3]) #steering angle
 #    if(np.abs(steering_angle)>0.1): 
     if(1): 
       source_path = line[0]
       filename = source_path.split('/')[-1] #remove intermediate path
       image_file = data_path + 'IMG/' + filename
-      image = cv2.imread(image_file, cv2.COLOR_BGR2RGB)[crop_hgt:]
+      image = cv2.imread(image_file, cv2.COLOR_BGR2RGB)
       images.append(image)
 
       measurements.append(steering_angle)
 
     # left channel
-    if(np.random.random()<.1):
+    if(np.random.random()<.2):
 #    if(1):
       source_path = line[1]
       filename = source_path.split('/')[-1] #remove intermediate path
       image_file = data_path + 'IMG/' + filename
-      image = cv2.imread(image_file, cv2.COLOR_BGR2RGB)[crop_hgt:]
+      image = cv2.imread(image_file, cv2.COLOR_BGR2RGB)
       images.append(image)
 
       measurement = steering_angle + steer_corr #steering angle
@@ -44,12 +44,12 @@ with open(csv_file) as f:
       measurements.append(measurement)
 
     # right channel
-    if(np.random.random()<.1):
+    if(np.random.random()<.2):
 #    if(1):
       source_path = line[2]
       filename = source_path.split('/')[-1] #remove intermediate path
       image_file = data_path + 'IMG/' + filename
-      image = cv2.imread(image_file, cv2.COLOR_BGR2RGB)[crop_hgt:]
+      image = cv2.imread(image_file, cv2.COLOR_BGR2RGB)
       images.append(image)
 
       measurement = steering_angle - steer_corr #steering angle
@@ -68,57 +68,73 @@ y_train = np.concatenate((y_train, y_train_flip), axis=0)
 
 # train model  
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, MaxPooling2D, Dropout
+from keras.layers import Flatten, Dense, Lambda, MaxPooling2D, Dropout, Cropping2D
 from keras.layers.convolutional import Conv2D
 
+imshape = X_train[0].shape
+
 model = Sequential()
-imshape = images[0].shape
-model.add(Lambda(lambda x: (x/255.0)-0.5, input_shape=imshape))
+model.add(Lambda(lambda x: ((x/255.0)-0.5),
+                 input_shape=imshape,
+                 name='lambda'))
+model.add(Cropping2D(cropping=((50,20),(0,0)),
+                     name='crop'))
 model.add(Conv2D(nb_filter=24,
-                 nb_row=8,
-                 nb_col=8,
+                 nb_row=5,
+                 nb_col=5,
                  subsample=(2,2),
                  border_mode='valid',
-                 activation='elu'))
+                 activation='elu',
+                 name='conv1'))
 # model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Conv2D(nb_filter=36,
                  nb_row=5,
                  nb_col=5,
                  subsample=(2,2),
                  border_mode='valid',
-                 activation='elu'))
+                 activation='elu',
+                 name='conv2'))
 model.add(Conv2D(nb_filter=48,
                  nb_row=5,
                  nb_col=5,
 		 subsample=(2,2),
                  border_mode='valid',
-                 activation='elu'))
-
+                 activation='elu',
+                 name='conv3'))
 model.add(Conv2D(nb_filter=64,
-                 nb_row=5,
-                 nb_col=5,
+                 nb_row=3,
+                 nb_col=3,
                  subsample=(1,1),
                  border_mode='valid',
-                 activation='elu'))
+                 activation='elu',
+                 name='conv4'))
 model.add(Conv2D(nb_filter=64,
-                 nb_row=5,
-                 nb_col=5,
+                 nb_row=3,
+                 nb_col=3,
                  subsample=(1,1),
                  border_mode='valid',
-                 activation='elu'))
+                 activation='elu',
+                 name='conv5'))
 # model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Flatten())
-model.add(Dropout(0.3))
-model.add(Dense(120, activation='relu'))
-model.add(Dense(84, activation='relu'))
-model.add(Dense(1))
+model.add(Flatten(name='flat'))
+model.add(Dropout(0.5, name='dropout'))
+model.add(Dense(100, activation='elu', name='dense1'))
+model.add(Dense(50, activation='elu', name='dense2'))
+model.add(Dense(10, activation='elu', name='dense3'))
+model.add(Dense(1, name='output'))
+
+print('Layer shapes:')
+for layer in model.layers:
+  shape = layer.get_input_shape_at(0)
+  print(shape, layer.name)
+
 
 model.compile(loss='mse',
               optimizer='adam')
 model.fit(X_train, y_train,
           validation_split=0.2,
           shuffle=True,
-          nb_epoch=6,
+          nb_epoch=3,
           batch_size=64)
 
 model.save('model.h5')
