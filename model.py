f53e45fd3bca052d5a3eba4881f035b45639ba8d
AeroGeekDean <windsurf_dean@yahoo.com>
2017-04-09 00:04:15 -0700

    Updated model.py with jupyter notebook code

--- model.py
+++ model.py
@@ -2,77 +2,112 @@ import csv
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import sklearn
+from sklearn.model_selection import train_test_split
+
+#-----------------
+# Data processing
+#-----------------
 
 # Load data
+data_path = './training_data/' # AWS instance
 # data_path = './data/' # default sim data from Udacity
-# data_path = '../Simulator/' # my sim data
-data_path = './training_data/' # AWS sim data
-csv_file = data_path + 'driving_log.csv'
+# data_path = '../Simulator/' # my local sim data
+img_path = data_path+'IMG/'
+csv_file = data_path+'driving_log.csv'
 
-steer_corr = 0.05 # [normalized +/-1.0] (+) right
-
-images=[]
-measurements = []
-
+# read the CSV file into a list
+samples = []
 with open(csv_file) as f:
-  reader = csv.reader(f)
-  next(reader) # skip 1st line
-  for line in reader:
-    steering_angle = float(line[3])
+    reader = csv.reader(f)
+    next(reader) # skip 1st line, in case it's a header line
+    for line in reader:
+        samples.append(line)
 
-    # center channel
-#    if(np.abs(steering_angle)>0.1): 
-    if(1): 
-      source_path = line[0]
-      filename = source_path.split('/')[-1] #remove intermediate path
-      image_file = data_path + 'IMG/' + filename
-      image = cv2.imread(image_file, cv2.COLOR_BGR2RGB)
-      images.append(image)
+# split out the validation dataset
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-      measurements.append(steering_angle)
+# offset camera steering function
+# Attempt to provide more correction when steering AWAY from camera,
+# and less correction when towards camera
+def steering_corr(steer):
+    k = 1.0
+    b = 0.25
+#     return k*(np.exp(steer)-1) + b
+    return b # Aw crap, doesn't work. Just stick with bias then.
 
-    # left channel
-    if(np.random.random()<.2):
-#    if(1):
-      source_path = line[1]
-      filename = source_path.split('/')[-1] #remove intermediate path
-      image_file = data_path + 'IMG/' + filename
-      image = cv2.imread(image_file, cv2.COLOR_BGR2RGB)
-      images.append(image)
+#set image color space
+colorspace = cv2.COLOR_BGR2RGB
 
-      measurement = steering_angle + steer_corr #steering angle
-      measurement = min(measurement, 1.0)
-      measurements.append(measurement)
+# generator function that acturally reads the image(s), on demand / as needed
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+#         shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-    # right channel
-    if(np.random.random()<.2):
-#    if(1):
-      source_path = line[2]
-      filename = source_path.split('/')[-1] #remove intermediate path
-      image_file = data_path + 'IMG/' + filename
-      image = cv2.imread(image_file, cv2.COLOR_BGR2RGB)
-      images.append(image)
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
 
-      measurement = steering_angle - steer_corr #steering angle
-      measurement = max(measurement, -1.0)
-      measurements.append(measurement)
+                # center camera
+                img_name = img_path+batch_sample[0].split('/')[-1]
+                center_image = cv2.cvtColor(cv2.imread(img_name), colorspace)
+                center_angle = float(batch_sample[3])
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+                # left camera
+                img_name = img_path+batch_sample[1].split('/')[-1]
+                left_image = cv2.cvtColor(cv2.imread(img_name), colorspace)
+                left_angle = center_angle + steering_corr(center_angle)
+                left_angle = min(max(left_angle, -1.0), 1.0)
 
-# generate flipped image data
-X_train_flip = np.array(np.fliplr(images))
-y_train_flip = -np.array(measurements)
+                # right camera
+                img_name = img_path+batch_sample[2].split('/')[-1]
+                right_image = cv2.cvtColor(cv2.imread(img_name), colorspace)
+                right_angle = center_angle - steering_corr(-center_angle)
+                right_angle = min(max(right_angle, -1.0), 1.0)
 
-X_train = np.concatenate((X_train, X_train_flip), axis=0)
-y_train = np.concatenate((y_train, y_train_flip), axis=0)
+                # append data from cameras
+                images.append(center_image)
+                angles.append(center_angle)
+                images.append(left_image)
+                angles.append(left_angle)
+                images.append(right_image)
+                angles.append(right_angle)
+            
+            X_train_orig = np.array(images)
+            y_train_orig = np.array(angles)
+            
+            # create flipped image data as well
+            X_train_flip = np.array(np.fliplr(images))
+            y_train_flip =-np.array(angles)
+            
+            X_train = np.concatenate((X_train_orig, X_train_flip), axis=0)
+            y_train = np.concatenate((y_train_orig, y_train_flip), axis=0)
+            
+            yield sklearn.utils.shuffle(X_train, y_train)
 
-# train model  
+# instantiate generators
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+# determine the input image shape
+# by looking at 1st frame's center image
+img_name = img_path+train_samples[0][0].split('/')[-1]
+image0 = cv2.cvtColor(cv2.imread(img_name), colorspace)
+imshape = image0.shape
+
+#------------------
+# Model definition
+#------------------
+
+# based upon Nvidia paper:
+# http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
+
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, MaxPooling2D, Dropout, Cropping2D
 from keras.layers.convolutional import Conv2D
-
-imshape = X_train[0].shape
 
 model = Sequential()
 model.add(Lambda(lambda x: ((x/255.0)-0.5),
@@ -87,7 +122,6 @@ model.add(Conv2D(nb_filter=24,
                  border_mode='valid',
                  activation='elu',
                  name='conv1'))
-# model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Conv2D(nb_filter=36,
                  nb_row=5,
                  nb_col=5,
@@ -98,7 +132,7 @@ model.add(Conv2D(nb_filter=36,
 model.add(Conv2D(nb_filter=48,
                  nb_row=5,
                  nb_col=5,
-		 subsample=(2,2),
+                 subsample=(2,2),
                  border_mode='valid',
                  activation='elu',
                  name='conv3'))
@@ -116,7 +150,6 @@ model.add(Conv2D(nb_filter=64,
                  border_mode='valid',
                  activation='elu',
                  name='conv5'))
-# model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten(name='flat'))
 model.add(Dropout(0.5, name='dropout'))
 model.add(Dense(100, activation='elu', name='dense1'))
@@ -124,39 +157,38 @@ model.add(Dense(50, activation='elu', name='dense2'))
 model.add(Dense(10, activation='elu', name='dense3'))
 model.add(Dense(1, name='output'))
 
-from functools import reduce
-def prod(input):
-  return reduce(lambda x, y: x*y, input, 1)
+# model compilation
+model.compile(loss='mse', optimizer='adam')
 
-print('Layer input shapes:')
-for layer in model.layers:
-  shape = layer.get_input_shape_at(0)
-  print(layer.name, shape, ' = '+str(prod(shape[1:]))+' neurons', sep='\t')
+# set up Model checkpoint saving callback
+from keras.callbacks import ModelCheckpoint
 
+save_filename = 'model.h5'
+checkpoint = ModelCheckpoint(save_filename,
+                             monitor='val_loss',
+                             save_best_only=True,
+                             save_weights_only=False)
+callbacks_list = [checkpoint]
 
+# train the model
+history_obj =  model.fit_generator(train_generator,
+                                   samples_per_epoch=len(train_samples)*6,
+                                   validation_data=validation_generator,
+                                   nb_val_samples=len(validation_samples)*6,
+                                   nb_epoch=6,
+                                   callbacks=callbacks_list)
 
-model.compile(loss='mse',
-              optimizer='adam')
-history_obj = model.fit(X_train, y_train,
-                        validation_split=0.2,
-                        shuffle=True,
-                        nb_epoch=3,
-                        batch_size=64)
-
-model.save('model.h5')
-
-### print the keys contained in the history object
-print('History object keys:')
-print(history_obj.history.keys())
-
-### plot the training and validation loss for each epoch
-plt.plot(history_obj.history['loss'])
-plt.plot(history_obj.history['val_loss'])
+# plot the training and validation loss for each epoch
+plt.plot(history_obj.history['loss'],'x-')
+plt.plot(history_obj.history['val_loss'],'o-')
 plt.title('model mean squared error loss')
 plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
+plt.grid()
 plt.show()
 
+# print out model summary
+print(model.summary())
 
 exit()
